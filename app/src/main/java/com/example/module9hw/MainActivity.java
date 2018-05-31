86af5a2145e72d2315da2538d4e966dd4a2a2b83
Zezus <rustam-sadvakaso@mail.ru>
2018-05-31 17:05:27 +0600

    end

---
+++ app/src/main/java/com/example/module9hw/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.module9hw;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void load(View view) {
+        setAlarm().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> {
+                    if (result.resolveActivity(getPackageManager()) != null) startActivity(result);
+                }, throwable -> {
+                    Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private Observable<Intent> setAlarm() {
+        return Observable.fromCallable(() -> {
+            Thread.sleep(3000);
+            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                    .putExtra(AlarmClock.EXTRA_HOUR, 9)
+                    .putExtra(AlarmClock.EXTRA_MINUTES, 0);
+            return intent;
+        });
+    }
+}
